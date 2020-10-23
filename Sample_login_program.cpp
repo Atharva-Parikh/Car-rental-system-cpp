a1fa729eef8181033d872beaae1b9e99afffa831
Atharva Parikh <54139853+Atharva-Parikh@users.noreply.github.com>
2020-10-23 22:12:51 +0530

    Update Sample_login_program.cpp

--- Sample_login_program.cpp
+++ Sample_login_program.cpp
@@ -1,87 +1,81 @@
-//Sample login program:
 #include<iostream>
-#include<conio.h>
+#include<sstream>
 #include<fstream>
 #include<string.h>
+#include<conio.h>
 using namespace std;
-class Login
-{
-        /* File handling
-        username
-        password
-        role   teacher, student admin    */
-        protected:
-            char username[50];
-            char password[50];
-            unsigned int role;
-        public:
-            void Register()
-            {
-               
-                ofstream fs("logindata.txt",ios::app);   
-                cout<<"Enter username: ";
-                cin>>username;
-                cout<<"Enter password: ";
-                cin>>password;
-                cout<<"Press 1. Student 2. Faculty 3. Admin"<<endl;
-                cin>>role;
-                fs<<username<<" "<<password<<" "<<role<<" ";   
-                fs.close();
-            }
-            void login(char  un[],char pw[])
-            {
-                ifstream fs("logindata.txt",ios::in);
-                while(!fs.eof())
-                {
-                fs>>username>>password>>role;   
-               
-                if(strcmp(un,this->username)==0 && strcmp(pw,this->password)==0)
-                {
-                    if(role==1)
-                    {
-                        cout<<"Welcome to student : "<<username;
-                    }
-                    else if(role==2)
-                    {
-                        cout<<"Welcome to faculty : "<<username;
-                    }
-                    else if(role==3)
-                    {
-                        cout<<"Welcome to admin : "<<username;
-                    }
-                }
-           
-                }
-                fs.close();   
-            }
+
+class Login{ 
+	protected:		
+		char username[100];
+		char password[50];
+		unsigned int access;
+	public:
+		void Register();
+		void login(char user_n[], char pass[]);
 };
+
+void Login :: Register()
+{
+	ofstream fout("login_data.txt",ios::app);
+	cout<<"Enter username(mail): ";
+	cin>>username;
+	cout<<"Enter your preferred password: ";
+	cin>>password;
+	cout<<"Press 1. Member 2.Admin: ";
+	cin>>access;
+	fout<<username<<" "<<password<<" "<<access<<" ";
+	fout.close();
+}
+
+void Login :: login(char user_n[], char pass[])
+{
+	ifstream fin("login_data.txt",ios::in);
+	while(!fin.eof())
+	{
+		fin>>username>>password>>access;
+		if(strcmp(user_n,this->username)==0 && strcmp(pass,this->password)==0)
+		{
+			if(access==1)
+			{
+				cout<<"Welcome back "<<username<<endl;
+			}
+			else if(access==2)
+			{
+				cout<<"Another day at work, isn't it "<<username<<endl;
+			}
+		}
+	}
+	fin.close();
+}
+
+
+
 int main()
 {
-   
-    Login l1;
+	Login l1;
     while(1)
     {
-    system("cls");
-    char username[50],password[50];
-    int ch;
-    cout<<" ********  Welcome to SMS *********";
-    cout<<"\n 1. Login \n 2. Register 3. Exit"<<endl;
-    cout<<"Enter choice: ";
-    cin>>ch;
-    switch(ch)
-    {
-        case 1:
-           cout<<" Username : "; cin>>username;
-           cout<<" Password : "; cin>>password;
-           l1.login(username,password);
-        break;
-        case 2:
-                l1.Register();
-        break;
-        case 3:
-            exit(1);
-    }
-    getch();
+	    system("cls");
+	    char username[100],password[50];
+	    unsigned int ch;
+	    cout<<" ********  Welcome to Car Rental System *********";
+	    cout<<"\n 1. Login \n 2. Register \n 3. Exit"<<endl;
+	    cout<<"Enter choice: ";
+	    cin>>ch;
+	    switch(ch)
+	    {
+	        case 1:
+	           cout<<" Username : "; cin>>username;
+	           cout<<" Password : "; cin>>password;
+	           l1.login(username,password);
+	        break;
+	        case 2:
+	                l1.Register();
+	        break;
+	        case 3:
+	            exit(1);
+	    }
+	    getch();
   }
-  return 0;
 }
