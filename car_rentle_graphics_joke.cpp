95000ddc2de33eba79d6fe8a88fa0679b27c837d
rudra842001 <57897592+rudra842001@users.noreply.github.com>
2020-10-30 17:03:20 +0530

    Add files via upload

---
+++ car_rentle_graphics_joke.cpp
@@ -0,0 +1,7 @@
+#include <windows.h> 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+ LPSTR lpCmdLine, int nCmdShow) 
+{ 
+ MessageBox(NULL, "Yooo Bitchhh!!...wolcome to car_rentle_app", "Note", MB_OK); 
+ return 0; 
+} 
