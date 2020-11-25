f96689610a8279e0b4909fa778b3770d8593f405
Atharva Parikh <54139853+Atharva-Parikh@users.noreply.github.com>
2020-11-25 18:50:55 +0530

    Delete car_rentle_graphics_joke.cpp

--- car_rentle_graphics_joke.cpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h> 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
- LPSTR lpCmdLine, int nCmdShow) 
-{ 
- MessageBox(NULL, "welcome to car_rentle_app", "Note", MB_OK); 
- return 0; 
-} 
